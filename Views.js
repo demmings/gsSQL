a52a9c417c7ce8245b1bd3db9c0df0257a3f1b57
cdemmigs <cdemmings@gmail.com>
2022-08-07 17:34:54 -0400

    try again.

--- Views.js
+++ Views.js
@@ -1237,10 +1237,10 @@ class VirtualFields {
             let calculatedField = (typeof selField.terms == 'undefined') ? null : selField.terms;
 
             if (calculatedField == null && !this.hasField(columnName)) {
-                const functionNameRegex = /\w+(?=\()/;
+                const functionNameRegex = /^\w+\s*(?=\()/;
                 let matches = columnName.match(functionNameRegex)
                 if (matches != null && matches.length > 0)
-                    aggregateFunctionName = matches[0];
+                    aggregateFunctionName = matches[0].trim();
 
                 matches = SelectTables.parseForFunctions(columnName, aggregateFunctionName);
                 if (matches != null && matches.length > 1)
